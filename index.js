a6d4c11d19b87415043e45e33120339152f4a9b8
evolentDelight <arkanumbk@gmail.com>
2025-11-19 18:45:16 -0800

    implement base commander code

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
+import { program } from "commander";
 
+program
+  .name("expense-tracker")
+  .description("Command-Line Interface Application to manage expenses")
+  .version("0.0.1", "-v, --version", "output the current version");
+
+program.parse();

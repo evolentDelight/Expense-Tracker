5b69de1fef6b8758052a2edd307329346e6e1cad
evolentDelight <arkanumbk@gmail.com>
2025-11-26 18:07:09 -0800

    Add base code for the Add Expense command

--- index.js
+++ index.js
@@ -6,4 +6,18 @@ program
   .description("Command-Line Interface Application to manage expenses")
   .version("0.0.1", "-v, --version", "output the current version");
 
+// Command: Add
+program
+  .command("add")
+  .description("Add an expense into the tracker")
+  .requiredOption(
+    "-d, --description <name>",
+    "Assign a description or name of the expense"
+  )
+  .requiredOption("-a, --amount <number>", "Add an amount, cost, or expense")
+  .action(({ description, amount }) => {
+    console.log("Description: ", description);
+    console.log("Amount: ", amount);
+  });
+
 program.parse();
